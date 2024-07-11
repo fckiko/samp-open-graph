918c5ec5814b6bd67a363064c9d68c7bff1183d2
Francis <francis@serverless-secure.com-at-025181935248>
2024-07-12 04:05:10 +0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,8 +39,8 @@ export class AppComponent implements OnInit {
     this.meta.addTags([
       { property: 'og:title', content: 'My Angular Page' },
       { property: 'og:type', content: 'website' },
-      { property: 'og:url', content: 'https://www.example.com/page.html' },
-      { property: 'og:image', content: 'https://www.example.com/image.jpg' },
+      { property: 'og:url', content: 'https://dev.bidsnbuys.com' },
+      { property: 'og:image', content: 'https://img.jlmconsulting.services/fit-in/256x155/sls_1720675394886Image_1.png' },
       { property: 'og:description', content: 'This is a description of my Angular page.' }
     ]);
   }
