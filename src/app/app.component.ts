adbab985b667c7c5e6dd26648169da5f7887c29c
Francis <francis@serverless-secure.com-at-025181935248>
2024-07-12 03:25:12 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'samp-open-graph';
+// export class AppComponent {
+//   title = 'samp-open-graph';
+// }
+export class AppComponent implements OnInit {
+
+  constructor(private meta: Meta, private titleService: Title) { }
+
+  ngOnInit(): void {
+    this.setMetaTags();
+  }
+
+  setMetaTags(): void {
+    this.titleService.setTitle('My Angular Page');
+
+    this.meta.addTags([
+      { property: 'og:title', content: 'My Angular Page' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://www.example.com/page.html' },
+      { property: 'og:image', content: 'https://www.example.com/image.jpg' },
+      { property: 'og:description', content: 'This is a description of my Angular page.' }
+    ]);
+  }
 }
